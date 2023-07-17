a265ed9b4116d90f3d7b4c5ccb41c3dd73cbdb4e
Andrea Soares <asoaresdev@gmail.com>
2023-07-17 18:04:26 -0300

    feat:tipagens

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export type User = {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    createdAt: string
+}
+
+export type Products = {
+    id: string,
+    name: string,
+    price: number,
+    description: string,
+    imageUrl: string
+}
